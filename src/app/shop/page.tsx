b1447e1cc1ad960daabcd72a59991174f5ca05ae
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-09-12 21:11:36 -0300

    /shop page

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { titleFont } from "../../fonts/fonts";
+
+export default function Shop() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>Hola mundo</h1>
+        <h1 className={titleFont.className}>Hola mundo</h1>
+      </main>
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  );
+}
