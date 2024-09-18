965bc630204aa4a6c1e38c43cd14989e81ad6cb6
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-09-18 20:49:14 -0300

    ProductGridItem image changing

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import { useState } from "react";
 import { Product } from "@/interfaces";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,16 +11,20 @@ interface Props {
 }
 
 export const ProductGridItem = ({ product }: Props) => {
+  const [displayImage, setDisplayImage] = useState(product.images[0]);
+
   return (
     <div>
-        <Link href={`product/${product.slug}`}>
+      <Link href={`product/${product.slug}`}>
         <Image
-        src={`/products/${product.images[0]}`}
-        alt={product.title}
-        width={250}
-        height={250}
-      />
-        </Link>
+          src={`/products/${displayImage}`}
+          alt={product.title}
+          width={250}
+          height={250}
+          onMouseEnter={()=>{setDisplayImage(product.images[1])}}
+          onMouseLeave={()=>{setDisplayImage(product.images[0])}}
+        />
+      </Link>
       <div>
         <Link href={`product/${product.slug}`}>{product.title}</Link>
         <span>${product.price}</span>
