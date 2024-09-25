ef5b4b323f032e21df2c6c9e172a26592df30eb3
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-09-25 15:02:05 -0300

    checkout/address page

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,7 +1,63 @@
+import { Title } from "@/components";
+import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function () {
-    return (
-        <div>
-            <h1>Address Page</h1>
+  return (
+    <div className={styles.container}>
+      <div>
+        <Title title="Dirección" subtitle="Dirección de entrega" />
+
+        <div className={styles.formContainer}>
+            
+          <div className={styles.inputContainer}>
+            <span>Nombres</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>Apellidos</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>Dirección</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>Dirección 2 (opcional)</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>Código postal</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>Ciudad</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>País</span>
+            <select>
+              <option value="">[ Seleccione ]</option>
+              <option value="CRI">Argentina</option>
+            </select>
+          </div>
+
+          <div className={styles.inputContainer}>
+            <span>Teléfono</span>
+            <input type="text" />
+          </div>
+
+          <div className={styles.inputContainer}>
+            <Link href="/checkout">Siguiente</Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,35 @@
+import { Title } from "@/components";
+import Link from "next/link";
+import styles from "./page.module.css";
+import ProductsGridCart from "@/components/products/product-grid-cart/ProductsGridCart";
+
+
+
+
 export default function () {
-    return (
-        <div>
-            <h1>Checkout Page</h1>
+
+  return (
+    <div className={styles.container}>
+      <Title title="Carrito" />
+      <div className={styles.carritoContainer}>
+
+        <div className={styles.carrito}>
+          <ProductsGridCart />
+          <Link href="/" className={styles.keepShoping}>Continúa comprando</Link>
         </div>
-    )
+
+        <div className={styles.checkOut}>
+          <h2>Resumen de orden</h2>
+          <p>Número de productos <span>3</span></p>
+          <p>Subtotal <span>$150</span></p>
+          <p>Impuestos (21%)<span></span></p>
+          <p>Envío <span>$50</span></p>
+          <div className={styles.spacer}/>
+          <p className={styles.totalPrice}>Total <span>$500</span></p>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
