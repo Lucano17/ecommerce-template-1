fe072cdb935e79cbdb4a8f09b9ccee0241bb5663
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-01 13:44:56 -0300

    seed-database.ts categoriesMap

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,21 +1,28 @@
 import { initialData } from "./seed";
 import prisma from "../lib/prisma"
 
-async function main(){
+async function main() {
 
     // 1. Delete previous registers.
-    await Promise.all ([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
+    await Promise.all([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany(),
     ])
-    
+
     // Categories
-    const {categories, products} = initialData
-    const categoriesData = categories.map((name)=>({name}))
+    const { categories, products } = initialData
+    const categoriesData = categories.map((name) => ({ name }))
     await prisma.category.createMany({
         data: categoriesData
-    })
+    });
+
+    const categoriesDB = await prisma.category.findMany();
+
+    const categoriesMap = categoriesDB.reduce( (map, category) => {
+        map[ category.name.toLowerCase()] = category.id;
+        return map;
+      }, {} as Record<string, string>); //<string=shirt, string=categoryID>
 
 
 
@@ -27,7 +34,7 @@ async function main(){
 
 
 
-(()=>{
+(() => {
     if (process.env.NODE_ENV === "production") return
-    main( )
+    main()
 })();
