bc49275769a84175d2c20ed59658eb14841f0229
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-01 18:08:09 -0300

    <ProductGrid/> connected with database

---
+++ src/actions/products/product-pagination.ts
@@ -0,0 +1,35 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+
+
+export const getPaginatedProductsWithImages = async()=> {
+
+
+    try {
+        const products = await prisma.product.findMany({
+            // take: 6,
+            include: {
+                ProductImage: {
+                    take: 2,
+                    select: {
+                        url: true
+                    }
+                }
+            }
+        })
+
+
+        return {
+            currentPage: 1,
+            totalPages: 10,
+            products: products.map( product => ({
+                ...product,
+                images: product.ProductImage.map(image => image.url)
+            }))
+        }
+
+    } catch (error) {
+        throw new Error ("No se pudieron cargar los productos")
+    }
+}
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,15 @@
 import { ProductGrid, Title } from "@/components";
-import { initialData } from "@/seed/seed";
+// import { initialData } from "@/seed/seed";
 import styles from "./page.module.css"
+import { getPaginatedProductsWithImages } from "@/actions";
 
-const products = initialData.products;
+// const products = initialData.products;
 
-export default function Shop() {
+export default async function Shop() {
+
+  const {products} = await getPaginatedProductsWithImages()
+
+
   return (
     <>
       <Title className={styles.title} title="Tienda" subtitle="Todos los productos" />
