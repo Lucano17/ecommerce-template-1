0d056ef9e7b639350176fe4d5a8ea590873aa629
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-04 12:49:28 -0300

    Working with metadata

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,10 +1,36 @@
 // page.tsx (Página del servidor)
+import { getProductBySlug } from "@/actions";
 import { ProductServerComponent } from "@/components/server-client-components/server-component/product-server-component";
+import { Metadata, ResolvingMetadata } from "next";
 
 interface Props {
   params: {
     slug: string;
   };
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+ 
+  // fetch data
+  const product = await getProductBySlug(slug)
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title: product?.title ?? "Producto no encontrado",
+    description: product?.description ?? "",
+    openGraph: {
+      title: product?.title ?? "Producto no encontrado",
+      description: product?.description ?? "",
+      images: [`/products/${product?.images[1]}`],
+    },
+  }
 }
 
 export default async function ProductBySlugPage({ params }: Props) {
