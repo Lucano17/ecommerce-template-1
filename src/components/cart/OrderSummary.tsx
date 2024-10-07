36876c56eee72931e4fbef6b956e5593221d606d
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-07 18:47:03 -0300

    currencyFormat.ts

--- src/components/cart/OrderSummary.tsx
+++ src/components/cart/OrderSummary.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import styles from "./OrderSummary.module.css";
 import Link from "next/link";
 import { useCartStore } from "@/store";
+import { currencyFormat } from "@/utils";
 
 export const OrderSummary = () => {
   const [loaded, setLoaded] = useState(false)
@@ -21,12 +22,12 @@ export const OrderSummary = () => {
   return (
     <div className={styles.checkOut}>
       <h2>Resumen de orden</h2>
-      <p>Número de artículos<span>{itemsInCart}</span></p>
-      <p>Subtotal <span>{subtotal}</span></p>
-      <p>Impuestos (21%)<span>{tax}</span></p>
+      <p>Número de artículos<span>{currencyFormat(itemsInCart)}</span></p>
+      <p>Subtotal <span>{currencyFormat(subtotal)}</span></p>
+      <p>Impuestos (21%)<span>{currencyFormat(tax)}</span></p>
       <p>Envío <span>$50</span></p>
       <div className={styles.spacer} />
-      <p className={styles.totalPrice}>Total<span>{total}</span>
+      <p className={styles.totalPrice}>Total<span>{currencyFormat(total)}</span>
       </p>
       <div className={styles.goToOrder}>
         <Link href="/checkout/address">Checkout</Link>
