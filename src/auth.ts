043eebc9866ec4ca8ae0675b7168c90d92e10497
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-08 15:44:01 -0300

    installing zod & config providers

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,7 @@
 import type { NextAuthConfig } from 'next-auth';
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import {z} from "zod"
  
 export const authConfig = {
   pages: {
@@ -6,6 +9,20 @@ export const authConfig = {
     newUser: '/auth/new-account',
   },
   providers: [
-    
+    Credentials({
+        async authorize(credentials) {
+          const parsedCredentials = z
+            .object({ email: z.string().email(), password: z.string().min(6) })
+            .safeParse(credentials);
+
+            if (!parsedCredentials.success) return null
+            const {email, password} = parsedCredentials.data
+
+
+            return null
+        },
+      }),
   ]
 } satisfies NextAuthConfig;
+
+export const {signIn, signOut, auth} = NextAuth(authConfig)
