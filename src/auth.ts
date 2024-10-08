879eca6e6076739a2e66fe6467ff405d95d011c7
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-08 15:35:46 -0300

    installing NextAuth & creating auth.ts

---
+++ src/auth.ts
@@ -0,0 +1,11 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/new-account',
+  },
+  providers: [
+    
+  ]
+} satisfies NextAuthConfig;
