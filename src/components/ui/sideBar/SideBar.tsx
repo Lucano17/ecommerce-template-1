ee5458a662117e8c6f5dc766ff9f94dff55af774
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-11 12:56:25 -0300

    ProfilePage.tsx component

--- src/components/ui/sideBar/SideBar.tsx
+++ src/components/ui/sideBar/SideBar.tsx
@@ -15,80 +15,74 @@ import Link from "next/link";
 import { useUIStore } from "@/store";
 
 export const SideBar = () => {
-  const isSideMenuOpen = useUIStore(state => state.isSideMenuOpen);
-  const closeSideMenu = useUIStore(state => state.closeSideMenu);
+  const isSideMenuOpen = useUIStore((state) => state.isSideMenuOpen);
+  const closeSideMenu = useUIStore((state) => state.closeSideMenu);
 
   return (
     <div>
-      {
-        isSideMenuOpen && (
-          <div className={styles.blackBackground}></div>
-        )
-      }
-      {
-        isSideMenuOpen && (
-          <div className={styles.blurBackground}
-          onClick={closeSideMenu}></div>
-        )
-      }
+      {isSideMenuOpen && <div className={styles.blackBackground}></div>}
+      {isSideMenuOpen && (
+        <div className={styles.blurBackground} onClick={closeSideMenu}></div>
+      )}
 
       <div>
-      {
-        isSideMenuOpen && (
+        {isSideMenuOpen && (
           <nav className={styles.sideBar}>
-        <IoCloseOutline
-          className={styles.closeIcon}
-          onClick={closeSideMenu}
-        />
-        <div className={styles.searchContainer}>
-          <IoSearchOutline className={styles.searchIcon} size={20} />
-          <input
-            className={styles.searchInput}
-            type="text"
-            placeholder="Buscar"
-          />
-        </div>
+            <IoCloseOutline
+              className={styles.closeIcon}
+              onClick={closeSideMenu}
+            />
+            <div className={styles.searchContainer}>
+              <IoSearchOutline className={styles.searchIcon} size={20} />
+              <input
+                className={styles.searchInput}
+                type="text"
+                placeholder="Buscar"
+              />
+            </div>
 
-        <Link href={"/"} className={styles.navLinks}>
-          <IoPersonOutline className={styles.navIcon} size={25} />
-          <span className={styles.navLinksText}>Perfil</span>
-        </Link>
+            <Link
+              href={"/profile"}
+              className={styles.navLinks}
+              onClick={closeSideMenu}
+            >
+              <IoPersonOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Perfil</span>
+            </Link>
 
-        <Link href={"/orders"} className={styles.navLinks}>
-          <IoTicketOutline className={styles.navIcon} size={25}/>
-          <span className={styles.navLinksText}>Orders</span>
-        </Link>
+            <Link href={"/orders"} className={styles.navLinks}>
+              <IoTicketOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Orders</span>
+            </Link>
 
-        <Link href={"/"} className={styles.navLinks}>
-          <IoLogInOutline className={styles.navIcon} size={25}/>
-          <span className={styles.navLinksText}>Ingresar</span>
-        </Link>
+            <Link href={"/"} className={styles.navLinks}>
+              <IoLogInOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Ingresar</span>
+            </Link>
 
-        <Link href={"/"} className={styles.navLinks}>
-          <IoLogOutOutline className={styles.navIcon} size={25}/>
-          <span className={styles.navLinksText}>Salir</span>
-        </Link>
+            <Link href={"/"} className={styles.navLinks}>
+              <IoLogOutOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Salir</span>
+            </Link>
 
-        <div className={styles.navMidLine}/>
+            <div className={styles.navMidLine} />
 
-        <Link href={"/"} className={styles.navLinks}>
-          <IoShirtOutline className={styles.navIcon} size={25}/>
-          <span className={styles.navLinksText}>Productos</span>
-        </Link>
+            <Link href={"/"} className={styles.navLinks}>
+              <IoShirtOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Productos</span>
+            </Link>
 
-        <Link href={"/orders"} className={styles.navLinks}>
-          <IoTicketOutline className={styles.navIcon} size={25}/>
-          <span className={styles.navLinksText}>Órdenes</span>
-        </Link>
+            <Link href={"/orders"} className={styles.navLinks}>
+              <IoTicketOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Órdenes</span>
+            </Link>
 
-        <Link href={"/"} className={styles.navLinks}>
-          <IoPeopleOutline className={styles.navIcon} size={25}/>
-          <span className={styles.navLinksText}>Usuarios</span>
-        </Link>
-
-      </nav>
-        )
-      }
+            <Link href={"/"} className={styles.navLinks}>
+              <IoPeopleOutline className={styles.navIcon} size={25} />
+              <span className={styles.navLinksText}>Usuarios</span>
+            </Link>
+          </nav>
+        )}
       </div>
     </div>
   );
