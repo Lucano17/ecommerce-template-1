7404d89bafce7d01d4c1175d5bd98754c3e56ca5
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-12 13:19:18 -0300

    login / logout backup

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -1,44 +1,3 @@
-
-// "use server";
-
-// import { signIn } from "@/auth.config";
-// import { redirect } from "next/navigation"; 
-
-// export async function authenticate(formData: FormData) {
-//   try {
-//     const email = formData.get('email') as string;
-//     const password = formData.get('password') as string;
-
-//     if (!email || !password) {
-//       return { error: "Email y contraseña son requeridos." };
-//     }
-
-//     const response = await signIn('credentials', {
-//       email,
-//       password,
-//       redirect: false, // Evita redirección automática
-//     });
-
-//     if (response?.error) {
-//       // Devolvemos el error específico de la respuesta de NextAuth
-//       return { error: response.error };
-//     }
-
-//     if (!response?.ok) {
-//       // Si la respuesta no es OK, algo salió mal
-//       return { error: "Error de autenticación. Por favor, revisa tus credenciales." };
-//     }
-
-//     // Si es exitoso, redirigir al usuario
-//     redirect('/');
-
-//   } catch (error) {
-//     console.error("Error en authenticate:", error);
-//     return { error: "Error inesperado, por favor intenta nuevamente." };
-//   }
-// }
-
-
 'use server';
 
 
@@ -52,14 +11,20 @@ export async function authenticate(
   try {
 
     // await sleep(2);
-    console.log("Object fromEntries:", Object.fromEntries(formData))
-    await signIn('credentials', Object.fromEntries(formData));
+    console.log("Object fromEntries:", Object.fromEntries(formData)) //TODO DELETE THIS
+    await signIn('credentials',{
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+
+    return 'Success'
 
 
   } catch (error) {
-
-    return 'CredentialsSignin'
-
+    if ((error as any).type === 'CredentialsSignin') {
+      return 'CredentialsSignin'
+    }
+    return 'Unknown error';
 
   }
 }
