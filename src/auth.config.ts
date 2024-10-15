fea7e58a01a95c95ed9a2a338d0eda6fb439f555
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-15 14:48:27 -0300

    auth.config session callback & Session interface

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,5 @@
 
-import NextAuth, {type NextAuthConfig} from 'next-auth';
+import NextAuth, { type NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import bcryptjs from "bcryptjs";
 import { z } from "zod"
@@ -10,46 +10,58 @@ export const authConfig = {
     signIn: '/auth/login',
     newUser: '/auth/new-account',
   },
+
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.data = user
+      }
+      return token
+    },
+    session({ session, token, user }) {
+      session.user = token.data as any;
+      return session
+    }
+  },
+
   providers: [
     Credentials({
       async authorize(credentials) {
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
-      
+
         console.log("Parsed Credentials:", parsedCredentials.success); // Log para ver las credenciales parseadas
-      
+
         if (!parsedCredentials.success) return null;
         const { email, password } = parsedCredentials.data;
-      
+
         const user = await prisma.user.findUnique({
           where: { email: email.toLowerCase() },
         });
-      
-        // console.log("User Found:", user); // Log para ver el usuario encontrado
-      
+
         if (!user) {
           console.log("No user found with this email."); // Log si no se encuentra usuario
           return null;
         }
-      
+
         if (!bcryptjs.compareSync(password, user.password)) return null
 
         // const isPasswordCorrect = await bcryptjs.compare(password, user.password);
         // console.log("Password Match:", isPasswordCorrect); // Log para ver si las contraseñas coinciden
-      
+
         // if (!isPasswordCorrect) {
         //   console.log("Incorrect password."); // Log si la contraseña es incorrecta
         //   return null;
         // }
-      
-        // Eliminar la contraseña del objeto usuario
-        const { password: _, ...rest } = user;
-        console.log({rest})
+
+       
+        const { password: _, ...rest } = user;  // Eliminar la contraseña del objeto usuario
+        console.log({ rest })
         return rest; // Devuelve el usuario sin la contraseña
       },
     }),
-  ] 
+  ]
 } satisfies NextAuthConfig;
 
 export const { signIn, signOut, auth, handlers } = NextAuth(authConfig)
