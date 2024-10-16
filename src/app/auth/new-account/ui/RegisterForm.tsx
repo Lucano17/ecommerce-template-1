3c3baf0e699e6c5ab29861c8674339d9114e82a0
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-16 15:22:51 -0300

    RegisterForm react-hook-form & name error

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -3,26 +3,51 @@
 import React from "react";
 import styles from "./RegisterForm.module.css";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormInputs = {
+    name: string;
+    email: string;
+    password: string;
+}
 
 export const RegisterForm = () => {
+
+    const {register, handleSubmit, formState: {errors}} = useForm<FormInputs>()
+    
+    const onSubmit: SubmitHandler<FormInputs> = async(data) =>{
+        const {name, email, password} = data
+    }
+
+
   return (
       <div className={styles.container}>
-        <div className={styles.formContainer}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
           <h1>Nueva cuenta</h1>
 
           <label htmlFor="name">Nombre completo</label>
-          <input placeholder="Juan Pérez" type="text" />
+          <input placeholder="Juan Pérez" type="text" 
+          {...register('name', { required: true })}
+          autoFocus/>
 
           <label htmlFor="email">Correo electrónico</label>
-          <input placeholder="juan.perez@gmail.com" type="email" />
+          <input placeholder="juan.perez@gmail.com" type="email"
+          {...register('email', { required: true })}/>
 
           <label htmlFor="password">Contraseña</label>
-          <input placeholder="**********" type="password" />
+          <input placeholder="**********" type="password"
+          {...register('password', { required: true })}/>
+
+          {
+            errors.name?.type === 'required' && (
+                <p>El nombre es obligatorio</p>
+            )
+          }
 
           <button>Registrarse</button>
 
           {/* divisor l ine */}
-        </div>
+        </form>
         <p className={styles.p}>Si ya tienes una cuenta</p>
         <Link href="/auth/login" className={styles.loginLink}>
           Ingresa
