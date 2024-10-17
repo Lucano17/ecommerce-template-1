93a16bd82782bf18d37d44bcdf77b6928e35ec82
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-17 12:44:47 -0300

    RegisterForm component error styles

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,57 +1,70 @@
-"use client"
+"use client";
 
 import React from "react";
 import styles from "./RegisterForm.module.css";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { IoInformationCircleOutline } from "react-icons/io5";
 
 type FormInputs = {
-    name: string;
-    email: string;
-    password: string;
-}
+  name: string;
+  email: string;
+  password: string;
+};
 
 export const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
 
-    const {register, handleSubmit, formState: {errors}} = useForm<FormInputs>()
-    
-    const onSubmit: SubmitHandler<FormInputs> = async(data) =>{
-        const {name, email, password} = data
-    }
-
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    const { name, email, password } = data;
+  };
 
   return (
-      <div className={styles.container}>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
-          <h1>Nueva cuenta</h1>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
+        <h1>Nueva cuenta</h1>
 
-          <label htmlFor="name">Nombre completo</label>
-          <input placeholder="Juan Pérez" type="text" 
-          {...register('name', { required: true })}
-          autoFocus/>
+        <label htmlFor="name">Nombre completo</label>
+        <input
+          placeholder="Juan Pérez"
+          type="text"
+          {...register("name", { required: true })}
+          autoFocus
+        />
 
-          <label htmlFor="email">Correo electrónico</label>
-          <input placeholder="juan.perez@gmail.com" type="email"
-          {...register('email', { required: true })}/>
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          placeholder="juan.perez@gmail.com"
+          type="email"
+          {...register("email", { required: true })}
+        />
 
-          <label htmlFor="password">Contraseña</label>
-          <input placeholder="**********" type="password"
-          {...register('password', { required: true })}/>
+        <label htmlFor="password">Contraseña</label>
+        <input
+          placeholder="**********"
+          type="password"
+          {...register("password", { required: true })}
+        />
 
-          {
-            errors.name?.type === 'required' && (
-                <p>El nombre es obligatorio</p>
-            )
-          }
+        {errors.name?.type === "required" && (
+          <p className={styles.formError}>
+            <IoInformationCircleOutline className={styles.errorIcon} /> El
+            nombre es obligatorio
+          </p>
+        )}
 
-          <button>Registrarse</button>
+        <button>Registrarse</button>
 
-          {/* divisor l ine */}
-        </form>
-        <p className={styles.p}>Si ya tienes una cuenta</p>
-        <Link href="/auth/login" className={styles.loginLink}>
-          Ingresa
-        </Link>
-      </div>
+        {/* divisor l ine */}
+      </form>
+      <p className={styles.p}>Si ya tienes una cuenta</p>
+      <Link href="/auth/login" className={styles.loginLink}>
+        Ingresa
+      </Link>
+    </div>
   );
 };
