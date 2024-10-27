ef68b9ac1b881fb5bb921dfe3c79b1eb05ecb3cf
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-27 17:02:58 -0300

    server action place-order.ts

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import { Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 
 interface ProductToOrder {
@@ -11,18 +12,31 @@ interface ProductToOrder {
 }
 
 
-export const placeOrder = async(productIds: ProductToOrder[], address: Address)=>{
+export const placeOrder = async (productIds: ProductToOrder[], address: Address) => {
     try {
         const session = await auth()
         const userId = session?.user.id
-        if (!userId){
+        if (!userId) {
             return {
                 ok: false,
                 message: "No hay sesión de usuario"
             }
         }
-        console.log({productIds,address, userId})
-    } catch (error){
+
+        const products = await prisma.product.findMany({
+            where: {
+              id: {
+                in: productIds.map((p) => p.productId),
+              },
+            },
+          });
+
+        console.log("products:", products)
+
+        const itemsInOrder = productIds.reduce((count, p)=> count + p.quantity, 0)
+
+
+    } catch (error) {
         return {
             ok: false,
             message: "Somthing went wrong"
