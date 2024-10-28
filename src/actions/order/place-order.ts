02e5934135808a0d7cc1a940a38e6edcd8f9f7da
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-10-28 17:54:04 -0300

    server action place-order subtotal, tax & total

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -11,6 +11,8 @@ interface ProductToOrder {
     size: Size
 }
 
+const taxIVARest = 0.21
+const taxIVA = 1.21
 
 export const placeOrder = async (productIds: ProductToOrder[], address: Address) => {
     try {
@@ -33,8 +35,26 @@ export const placeOrder = async (productIds: ProductToOrder[], address: Address)
 
         console.log("products:", products)
 
+        // Colocar los montos
         const itemsInOrder = productIds.reduce((count, p)=> count + p.quantity, 0)
 
+        // Colocar los totales de tax, subtotal y total
+        const {subtotal, tax, total} = productIds.reduce((totals, item) => {
+            const productQuantity = item.quantity;
+            const product = products.find(product => product.id === item.productId)
+
+            if (!product) throw new Error(`${item.productId} no existe - 500`)
+
+                const subtotal = product.price * productQuantity
+
+                totals.subtotal += subtotal
+                totals.tax += subtotal * taxIVARest
+                totals.total += subtotal * taxIVA
+
+                return totals
+        }, {subtotal: 0, tax:0, total:0})
+        
+        console.log({subtotal, tax, total})
 
     } catch (error) {
         return {
