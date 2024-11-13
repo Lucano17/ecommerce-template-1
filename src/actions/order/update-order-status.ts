f49536af59bee42e0350b3b8c0d30cf067459181
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-11-13 18:30:32 -0300

    update-order-status payment.data update to database TO-DO

--- src/actions/order/update-order-status.ts
+++ src/actions/order/update-order-status.ts
@@ -6,7 +6,14 @@ export const updateOrderStatus = async (orderId: string, paymentStatus: string) 
   try {
     await prisma.order.update({
       where: { id: orderId },
-      data: { status: paymentStatus },
+      data: {
+        status: paymentStatus,
+        // status_detailed: status_detailed,
+        // transactionId: id,
+        // paidAt: date_approved,
+        // currency_id: currency_id
+        // total_paid_amount: total_paid_amount
+      },
     });
   } catch (error) {
     console.error("Error updating order status:", error);
