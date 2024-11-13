e1775740b06ce58f088511331ba3a5fa125bdc4f
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-11-13 13:31:41 -0300

    mercadopago payment.GET webhook route clean code

--- src/app/api/payment/mercadopago/webhook/route.ts
+++ src/app/api/payment/mercadopago/webhook/route.ts
@@ -1,15 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { mercadopago, updateOrderStatus } from "@/actions";
-import MercadoPagoConfig from 'mercadopago';
-
-
 
 export async function POST(req: NextRequest) {
-
   try {
-
-    // const orderId = body.data.external_reference; // Usa `external_reference` para identificar la orden en tu sistema
-
     const { searchParams } = new URL(req.url);
     const paymentId = searchParams.get('data.id');
     const topic = searchParams.get('type');
@@ -22,32 +15,27 @@ export async function POST(req: NextRequest) {
     if (topic === "payment") {
       const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
         method: "GET",
-        headers: {
-          "Authorization": `Bearer ${mercadopago.accessToken}`
-        }
+        headers: { "Authorization": `Bearer ${mercadopago.accessToken}` }
       });
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Payment data from Mercado Pago:", data);
-
-        // Actualiza el estado de la orden en tu base de datos
-        await updateOrderStatus(data.external_reference, data.status);
-
-        // Responde a Mercado Pago con un estado 200 para confirmar que recibí el webhook
-        return NextResponse.json({ message: "Webhook received and processed" });
-      } else {
+      if (!response.ok) {
         console.log("No se ha logrado conectar correctamente con el webhook de Mercado Pago");
         return NextResponse.json({ error: "Failed to connect to Mercado Pago" }, { status: 500 });
       }
-    } else if (topic === "merchant_order") {
-      // Aquí manejo el caso para `merchant_order` si es necesario
-      console.log("Notificación recibida para merchant_order, no se procesa en este endpoint.");
-      return NextResponse.json({ message: "Notificación de merchant_order recibida" });
-    } else {
-      console.log("Tipo de notificación no soportado.");
-      return NextResponse.json({ error: "Tipo de notificación no soportado" }, { status: 400 });
-    }
+
+      const data = await response.json();
+      console.log("Payment data from Mercado Pago:", data);
+      // Update to Database
+      await updateOrderStatus(data.external_reference, data.status);
+
+      return NextResponse.json({ message: "Webhook received and processed" });
+    } 
+
+    console.log(topic === "merchant_order" ? "Notificación de merchant_order recibida" : "Tipo de notificación no soportado.");
+    return NextResponse.json(
+      { message: topic === "merchant_order" ? "Notificación de merchant_order recibida" : "Tipo de notificación no soportado" }, 
+      { status: topic === "merchant_order" ? 200 : 400 }
+    );
 
   } catch (error) {
     console.error("Error en el webhook de Mercado Pago:", error);
