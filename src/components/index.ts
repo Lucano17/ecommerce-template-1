a34c5d387d5a872e94a45c2e45bef102f5b81ee5
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-11-26 17:06:47 -0300

    ProductImage component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,12 @@
 export * from "./cart/OrderSummary"
 export * from "./cart/payment/IsPaid"
 
-export * from "./product/quantity-selector/QuantitySelector"
 export * from "./product/size-selector/SizeSelector"
 export * from "./product/slideshow/ProductMobileSlideShow"
 export * from "./product/slideshow/ProductSlideShow"
 export * from "./product/stock-label/StockLabel"
-export * from "./products/product-grid-checkout/ProductsInCheckout"
+export * from "./product/product-image/ProductImage"
+export * from "./product/quantity-selector/QuantitySelector"
 
 export * from "./products/product-grid-cart/ProductsInCart"
 export * from "./products/product-grid-checkout/ProductsInCheckout"
@@ -23,8 +23,8 @@ export * from "./ui/sideBar/SideBar"
 export * from "./ui/title/Title"
 export * from "./ui/top-menu/TopMenu"
 
-export * from "./cart/payment/payment-methods/PayPalButton"
 export * from "./cart/payment/payment-methods/MercadoPagoButton"
+export * from "./cart/payment/payment-methods/PayPalButton"
 
 export * from "./skeleton/Skeleton"
 
