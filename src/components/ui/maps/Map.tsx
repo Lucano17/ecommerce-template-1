203bc1c049595dc340af3931836cbbf186653c5c
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-12-10 10:54:40 -0300

    Map component windowDimention

--- src/components/ui/maps/Map.tsx
+++ src/components/ui/maps/Map.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useEffect, useState } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 // Interfaz para las propiedades del componente
 interface MapProps {
@@ -10,11 +10,37 @@ interface MapProps {
 }
 
 // Componente Map
-export const Map: React.FC<MapProps> = ({ lat = -34.603722, lng = -58.381592 }) => {
+export const Map: React.FC<MapProps> = ({
+  lat = -34.603722,
+  lng = -58.381592,
+}) => {
   const containerStyle = {
-    width: '100%',
-    height: '400px', // Ajusta el tamaño según tu diseño
+    width: "100%",
+    height: "400px", // Ajusta el tamaño según tu diseño
   };
+
+  const [windowDimention, setWindowDimention] = useState({ width: 0, height: 0 });
+  const [mapFull, setMapFull] = useState(true)
+
+  useEffect(()=>{
+    const detectDimention = () => {
+      setWindowDimention({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    detectDimention();
+    window.addEventListener("resize", detectDimention);
+    window.addEventListener("resize", detectDimention);
+    return () => {
+      window.removeEventListener("resize", detectDimention);
+    };
+
+  },[])
+
+  useEffect(() => {
+    if (windowDimention.width > 750) setMapFull(true);
+  }, [windowDimention]);
 
   return (
     // <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
@@ -24,15 +50,16 @@ export const Map: React.FC<MapProps> = ({ lat = -34.603722, lng = -58.381592 }) 
     // </LoadScript>
 
     <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105056.99950138609!2d-58.45458805935386!3d-34.62865096625379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1733527094295!5m2!1ses-419!2sar"
-    width="80%"
-    height="400"
-    style={{
-      border: 0, // Esto se define como un objeto
-    }}
-    allowFullScreen
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-  ></iframe>
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105056.99950138609!2d-58.45458805935386!3d-34.62865096625379!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcca3b4ef90cbd%3A0xa0b3812e88e88e87!2sBuenos%20Aires%2C%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1733527094295!5m2!1ses-419!2sar"
+      
+      width= {windowDimention.width > 750 ? "80%" : "100%"}
+      height="400"
+      style={{
+        border: 0, // Esto se define como un objeto
+      }}
+      allowFullScreen
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    ></iframe>
   );
 };
