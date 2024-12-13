f08410eee09ee4818e25dda7fd03e867c6758f78
Lucas De La Fuente <lucasdlf17@gmail.com>
2024-12-12 21:41:47 -0300

    webhook/route.ts NEXT_PUBLIC_MERCADO_PAGO_ACCESS_TOKEN

--- src/app/api/payment/mercadopago/webhook/route.ts
+++ src/app/api/payment/mercadopago/webhook/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
     if (topic === "payment") {
       const response = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
         method: "GET",
-        headers: { "Authorization": `Bearer ${mercadopago.accessToken}` }
+        headers: { "Authorization": `Bearer ${process.env.NEXT_PUBLIC_MERCADO_PAGO_ACCESS_TOKEN}` }
       });
 
       if (!response.ok) {
